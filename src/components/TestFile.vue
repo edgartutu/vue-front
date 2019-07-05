1d29b464a1dddf6f394b8acbb4c7286b21b04e0d
edgartutu <walugembe.john@gmail.com>
2019-07-05 20:44:03 +0300

    [egdar&omach] complete front end

---
+++ src/components/TestFile.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-toolbar>
+    <v-toolbar-title>{{title}}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      
+      <v-icon>person</v-icon>
+     
+      
+    </v-toolbar-items>
+  </v-toolbar>
+    
+</template>
+<script>
+export default {
+    data(){
+        return{
+            title:"Admin"
+        }
+    },
+    name:"App"
+}
+</script>
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <Button/><br>
+  <p></p>
+  <Signup/>
+</div>
+</template>
+<script>
+  import Button from '@/components/TestFile.vue'
+  import Signup from '@/components/SignUp.vue'
+  export default {
+    components: {
+      Button,
+      Signup
+    }
+    
+  }
+ 
+</script>
